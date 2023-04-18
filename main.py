db1da788860ee03c3cc10d45ef3289c1f83bd37b
ori_b <ori@ori>
2023-04-18 09:18:27 +0300

    changing routes

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from flask import render_template
 
 app = flask.Flask(__name__)
 
+
 @app.route("/")
-def index():
+def index():    
+    return "Hi"
+
+
+@app.route("/html")
+def serve_html():
     students = ['Moshe', 'David', 'Yosef', 'Shalom']
     return render_template("index.html", name="ori", address="Jerusalem", students=students)    
 
